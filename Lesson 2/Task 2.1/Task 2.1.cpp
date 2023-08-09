3692d3b0d30a0ebf4a0b3ef4077bb0f3639dc8ef
MaksimCPP8 <maksgreat1978@gmail.com>
2023-08-09 22:46:53 +0400

    Added Task 2.1 to the Lesson 2 folder

---
+++ Lesson 2/Task 2.1/Task 2.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+struct point 
+{
+	int m_x;
+	int m_y;	
+};
+
+void print_point(const point& point_object) 
+{
+	std::cout << "x: " << point_object.m_x << ", y: "
+		<< point_object.m_y << std::endl;
+}
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача №1. Программа с ошибками.\n\n";
+
+	point my_point;
+
+	for (int i = 0; i < 5; i++)
+	{
+		my_point.m_x=i;
+		my_point.m_y = i * 2;
+		print_point(my_point);
+	}
+	return 0;
+}
