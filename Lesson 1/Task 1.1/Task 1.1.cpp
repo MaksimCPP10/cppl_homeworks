a26d0d496eb689e5b9aebe1642c25b07cbef0fdb
MaksimCPP8 <maksgreat1978@gmail.com>
2023-08-09 22:01:55 +0400

    Added Tasks 1.1, 1.2 to the Lesson 1 folder

---
+++ Lesson 1/Task 1.1/Task 1.1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <functional>
+
+
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача №1. Лямбда-функция.\n";
+
+    std::vector<int> v1 {4, 7, 9, 14, 12};
+
+    std::cout << "\nВходные данные: ";
+
+    for (auto a : v1)
+    {
+        std::cout << a << " ";
+    }
+
+    std::cout << "\nВыходные данные: ";
+
+    auto lambda=[](std::vector<int> &vec)
+    {
+        for (auto &a : vec)
+        {
+            if (a % 2 == 1)
+            {
+                a *= 3;
+                std::cout << a << " ";
+            }
+            else
+            {
+                std::cout << a << " ";
+            }
+        }
+
+    };
+
+    lambda(v1);
+
+    std::cout << "\n";    
+}
---
+++ Lesson 1/Task 1.2/Task 1.2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <variant>
+
+std::variant<int, std::string, std::vector<int>> get_variant() 
+{
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача №2. std::variant.\n";
+
+	auto result = get_variant();
+
+	int index = result.index();
+	
+	if (index == 0)
+	{
+		auto output = std::get<int>(result);
+		std::cout << "\nРезультатом работы get_variant() является число: " << output * 2 << "\n";
+	}
+	else if(index==1)
+	{
+		auto output = std::get<std::string>(result);
+		std::cout << "\nРезультатом работы get_variant() является строка: \"" << output << "\"\n";
+	}
+	else
+	{
+		auto output = std::get<std::vector<int>>(result);
+		std::cout << "\nРезультатом работы get_variant() является vector: ";
+		for (int val : output)
+			std::cout << val << " ";	
+	}
+
+	std::cout << "\n";
+}
