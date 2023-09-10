2ff0f9fb9ad732ae814485df8454999b57a655e9
MaksimCPP8 <maksgreat1978@gmail.com>
2023-09-10 23:27:00 +0400

    Added Task 7.1 to the Lesson 7 folder

---
+++ Lesson 7/Task 7.1/Task 7.1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+void sort_map(std::map<char, int>& S)
+{
+    std::vector < std::pair <char, int> > vec;
+    for (auto& it : S)
+        vec.push_back(it);
+
+    sort (vec.begin(), vec.end());
+
+    for (auto& it : vec)
+    {
+        std::cout << "\n" << "\'" << it.first << "\'" << " :  " << it.second;
+    }
+}
+
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача №1. Частота символов.\n";
+
+    std::string phrase = { "Hello world!!" };
+
+    std::cout << "\n[IN]:\t" << phrase;
+
+    std::cout << "\n[OUT]:";
+
+    std::map<char, int> symb;
+    
+    for (auto it : phrase)
+    {
+        symb[it]++;        
+    } 
+       
+    //sort_map(symb);
+    /*
+    for (auto& it : symb)
+    {
+        std::cout << "\n" << "\'" << it.first << "\'" << " :  " << it.second;
+    }
+   */
+    for (auto it=symb.begin(); it!=symb.end(); it++)
+    {
+        std::cout << "\n" << "\'" << it->first << "\'" << " :  " << it->second;
+    }
+    
+
+}
