204aefaa07028066ee290094228698ed0a92ade9
MaksimCPP8 <maksgreat1978@gmail.com>
2023-09-13 23:15:30 +0400

    Added Tasks 8.1 and 8.2 to the Lesson 8 folder

---
+++ Lesson 8/Task 8.1/Task 8.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void unique(std::vector<int> &v)
+{
+    std::sort(v.begin(), v.end());
+    auto result = std::unique(begin(v), end(v));
+    v.erase(result, v.end());
+}
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача 1. Уникальный вектор.\n";
+
+    std::vector<int> vec{ 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::cout << "\n[IN]:\t";
+    for (auto& it : vec)
+    {
+        std::cout << it << " ";
+    }
+
+    unique(vec);
+
+    std::cout << "\n[OUT]:\t";
+    for (auto& it : vec)
+    {
+        std::cout << it << " ";
+    }
+
+}
---
+++ Lesson 8/Task 8.2/Task 8.2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+#include <string>
+
+template <class T>
+void print_container(T &cont)
+{
+    for (auto& it : cont)
+    {
+        std::cout << it << ", ";
+    }
+    std::cout<<"\b\b;\n";
+};
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача 2. Печать контейнера.\n";
+
+    std::cout << "\nПечать контейнера std::set:\t";
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::cout << "\nПечать контейнера std::list:\t";
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::cout << "\nПечать контейнера std::vector:\t";
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+
+}
