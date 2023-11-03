a09f2d58fe03bfc87e84092082346ee14d811998
MaksimCPP8 <maksgreat1978@gmail.com>
2023-11-03 22:07:46 +0400

    Added Tasks 10.1 and 10.2 to the Lesson 10 folder

---
+++ Lesson 9/Task 9.1/Task 9.1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+template <typename T>
+void move_vectors(std::vector<T> &vec1, std::vector<T> &vec2)
+{
+    std::vector<T> tmp = std::move(vec1);
+    vec1 = std::move(vec2);
+    vec2 = std::move(tmp);
+};
+
+template <class T>
+void print_container(T& cont)
+{
+    for (auto& it : cont)
+    {
+        std::cout << it << ", ";
+    }
+    std::cout << "\b\b;\n";
+};
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача 1. Перемещение объектов.\n";
+
+    std::vector <std::string> one = { "test_string1", "test_string2" };
+    std::vector <std::string> two;
+
+    std::cout << "\nСодержимое контейнера std::vector <std::string> one:\t";
+    print_container(one);
+
+    move_vectors(one, two);
+
+    std::cout << "\nСодержимое контейнера std::vector <std::string> two:\t";
+    print_container(two);
+
+    std::cout << "\n";
+
+}
---
+++ Lesson 9/Task 9.2/Task 9.2.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <string>
+#include <deque>
+#include <iterator>
+
+class big_integer
+{
+private:
+    std::string value;
+    std::deque<unsigned> result;
+
+public:
+    big_integer(){}
+    
+    big_integer(big_integer&& other) noexcept
+    {
+        this->value = other.value;
+        other.value = "";
+    }
+
+    big_integer& operator = (big_integer&& other) noexcept
+    {
+        if (&other == this) { return *this; }
+        this->value = other.value;
+        other.value = "";
+        return *this;
+    }
+
+    big_integer(std::string str) 
+    {
+        for (auto& n : str)
+            
+            dec.push_back(n-'0');
+        
+    };
+
+    big_integer& operator + (const big_integer& other)
+    {
+        big_integer sum;
+        auto size = std::max(dec.size(), other.dec.size());
+        if (dec.size() < size) 
+        { 
+            for (int i=0; i<(size - dec.size()); i++)
+            dec.push_front(0); 
+        }
+        if (other.dec.size() < size) 
+        { 
+            for (int i = 0; i < (size - other.dec.size()); i++)
+                other.dec.push_front();
+        }
+        for (auto i = size; i!=0; i--)
+        {
+            auto flag = 0;
+            auto a = i > dec.size() ? 0 : dec[i];
+            auto b = i > other.dec.size() ? 0 : other.dec[i];
+            result.push_back((a + b + flag) % 10);
+            flag = ((a + b + flag) / 10);
+
+        }
+        std::cout << "\n";
+        for (auto& i : result)
+            std::cout << i;
+
+        return result;
+    }
+       
+    big_integer operator * (const big_integer& other)
+    {
+
+    }
+    
+    big_integer(const big_integer& other)
+    {
+
+    }
+        
+    ~big_integer(){}
+
+};
+
+std::ostream& operator << (std::ostream& os, const big_integer& result)
+{    
+    return os << result;
+}
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача 2. Большие числа.\n";
+
+    auto number1 = big_integer("114575");
+    auto number2 = big_integer("78524");
+    auto result = number1 + number2;
+    std::cout << result;
+    
+}
